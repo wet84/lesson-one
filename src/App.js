e70d80dfe4460a5708b052a370ab8438dfc92e88
Vitalii <37340917+wet84@users.noreply.github.com>
2020-05-17 21:59:17 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  
+  render() {
+    const title = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+
+    return (
+      <div className="App">
+        <h1> {title }</h1>
+        <div className="text-section">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac dui ut mi laoreet porta id ac felis. 
+          Suspendisse potenti. Etiam diam magna, posuere vitae consectetur sed, viverra in neque.
+          Nulla malesuada vulputate odio. Etiam erat massa, ullamcorper rutrum lorem vel, pharetra consectetur tortor.
+          Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla facilisi.
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
